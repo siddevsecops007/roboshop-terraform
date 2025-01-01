3c5b8c4b8fa7b8670eaa4dd172d7c45c575e6b32
SathuSid <sisindhriy123@gmail.com>
2025-01-02 01:58:52 +0530

    Roboshop using terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "sg" {
     protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  egress {
+  ingress {
     from_port        = var.app_port
     to_port          = var.app_port
     protocol         = "TCP"
