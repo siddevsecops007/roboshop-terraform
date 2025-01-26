ea190d3454ab742ae6f6a396ca3041f9bdd36e93
SathuSid <sisindhriy123@gmail.com>
2025-01-26 15:54:41 +0530

    Kubernetes Cluster Setup.

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -14,6 +14,17 @@ resource "aws_iam_role" "eks-cluster-role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster-role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks-cluster-role.name
+}
+
 resource "aws_iam_role" "node-group-role" {
   name = "${var.env}-eks-node-group-role"
 
@@ -27,4 +38,19 @@ resource "aws_iam_role" "node-group-role" {
     }]
     Version = "2012-10-17"
   })
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-group-role.name
 }
