d4dc8e87fdb169c0959378a2fdb7a7cb60003ba0
SathuSid <sisindhriy123@gmail.com>
2025-01-28 23:39:37 +0530

    Kubernetes learn Helm

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "main" {
     subnet_ids = var.subnet_ids
   }
   access_config {
-    authentication_mode                         = "API_AND_CONFIG_MAP"
+      authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
   }
 }
