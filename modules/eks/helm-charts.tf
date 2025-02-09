3189d32ad06ec42dbc01c4f41ba570c44330231a
SathuSid <sisindhriy123@gmail.com>
2025-02-09 14:35:36 +0530

    Ingress

--- modules/eks/helm-charts.tf
+++ modules/eks/helm-charts.tf
@@ -1,6 +1,26 @@
+resource "null_resource" "kube-bootstrap" {
+  provisioner "local-exec" {
+    command = <<EOF
+aws eks update-kubeconfig --name ${var.env}-eks
+kubectl create namespace devops
+EOF
+  }
+
+}
+
+
+
+
 resource "helm_release" "nginx-ingress" {
+  depends_on = [null_resource.kube-bootstrap]
   chart = "oci://ghcr.io/nginx/charts/nginx-ingress"
   name  = "nginx-ingress"
+  namespace = "devops"
+  wait       = true
+
+  values = [
+    file("${path.module}/helm-config/nginx-ingress.yml")
+  ]
 }
 
 
