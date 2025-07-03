f20b84e9c0c35633ffa22604adee32b8226e729b
SathuSid <sisindhriy123@gmail.com>
2025-07-03 09:37:41 +0530

    roboshop again

--- main.tf
+++ main.tf
@@ -11,42 +11,42 @@ module "db_instaces" {
     volume_size    = each.value["volume_size"]
 }
 
-module "app_instaces" {
-    depends_on = [module.db_instaces]
-    for_each       = var.app_instances
-    source         = "./modules/ec2"
-    env            = var.env
-    instance_type  = each.value["instance_type"]
-    component_name = each.key
-    app_port       = each.value["app_port"]
-    domain_name    = var.domain_name
-    zoneid         = var.zoneid
-    vault_token    = var.vault_token
-    volume_size    = each.value["volume_size"]
-}
-
-module "web_db_instaces" {
-    depends_on = [module.app_instaces]
-    for_each       = var.web_instances
-    source         = "./modules/ec2"
-    env            = var.env
-    instance_type  = each.value["instance_type"]
-    component_name = each.key
-    app_port       = each.value["app_port"]
-    domain_name    = var.domain_name
-    zoneid         = var.zoneid
-    vault_token    = var.vault_token
-    volume_size    = each.value["volume_size"]
-}
-
-# module "eks" {
-#     source              = "./modules/eks"
-#     env                 = var.env
-#     subnet_ids          = var.eks["subnet_ids"]
-#     add-ons             = var.eks["add-ons"]
-#     node-groups         = var.eks["node-groups"]
-#     access-entries      = var.eks["access-entries"]
+# module "app_instaces" {
+#     depends_on = [module.db_instaces]
+#     for_each       = var.app_instances
+#     source         = "./modules/ec2"
+#     env            = var.env
+#     instance_type  = each.value["instance_type"]
+#     component_name = each.key
+#     app_port       = each.value["app_port"]
+#     domain_name    = var.domain_name
+#     zoneid         = var.zoneid
+#     vault_token    = var.vault_token
+#     volume_size    = each.value["volume_size"]
 # }
+#
+# module "web_db_instaces" {
+#     depends_on = [module.app_instaces]
+#     for_each       = var.web_instances
+#     source         = "./modules/ec2"
+#     env            = var.env
+#     instance_type  = each.value["instance_type"]
+#     component_name = each.key
+#     app_port       = each.value["app_port"]
+#     domain_name    = var.domain_name
+#     zoneid         = var.zoneid
+#     vault_token    = var.vault_token
+#     volume_size    = each.value["volume_size"]
+# }
+
+module "eks" {
+    source              = "./modules/eks"
+    env                 = var.env
+    subnet_ids          = var.eks["subnet_ids"]
+    add-ons             = var.eks["add-ons"]
+    node-groups         = var.eks["node-groups"]
+    access-entries      = var.eks["access-entries"]
+}
 
 
 ###
